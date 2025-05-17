3826149200a3be87c1423d2b58de0db9fe0308a9
itsEzz <github.swore664@passmail.net>
2025-05-17 16:20:41 +0200

    feat: Implement FormError component

---
+++ src/lib/components/common/form-error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { AlertCircle } from 'lucide-svelte';
+
+	// Props
+	interface Props {
+		errors?: string[];
+	}
+
+	let { errors }: Props = $props();
+</script>
+
+{#if errors && errors.length > 0}
+	<div class="ml-1 space-y-1">
+		{#each errors as error}
+			<p class="flex items-center text-sm font-medium text-destructive">
+				<AlertCircle class="mr-1.5 h-4 w-4" />
+				{error}
+			</p>
+		{/each}
+	</div>
+{/if}
