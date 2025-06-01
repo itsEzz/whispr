1ad111697e9a864a206e805fe0cd659374536bcb
itsEzz <github.swore664@passmail.net>
2025-06-01 13:58:43 +0200

    refactor: Move load logic to layout server

--- src/routes/v/[id]/+layout.server.ts
+++ src/routes/v/[id]/+layout.server.ts
@@ -2,13 +2,12 @@ import { viewPasswordSchema } from '$lib/schemas/view-schema';
 import { readWhispr } from '$lib/server/read-whispr';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import type { PageServerLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: LayoutServerLoad = async ({ params }) => {
 	const { id } = params;
 
 	const whispr = await readWhispr(id);
-
 	const form = await superValidate(zod(viewPasswordSchema), { errors: true });
 
 	return {
