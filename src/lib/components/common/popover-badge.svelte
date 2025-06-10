74e2b244c408d737dee7d6d2481b84dd15067a68
itsEzz <github.swore664@passmail.net>
2025-06-10 19:53:05 +0200

    feat: Improve PopoverBadge component styling

--- src/lib/components/common/popover-badge.svelte
+++ src/lib/components/common/popover-badge.svelte
@@ -36,7 +36,7 @@
 		aria-controls={popoverId}
 		aria-describedby={badgeId}
 	>
-		<Badge {variant} class={cn('h-7', classes)} id={badgeId}>
+		<Badge {variant} class={cn('gap-2 [&>svg]:!size-auto', classes)} id={badgeId}>
 			{#if typeof content === 'string'}
 				{content}
 			{:else}
