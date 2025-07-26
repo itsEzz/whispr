9a39525bd9076a4dcf2a6170f4fa1851e66fb746
itsEzz <github.swore664@passmail.net>
2025-07-26 15:53:32 +0200

    Moved no password set alert to owm component

---
+++ src/lib/components/create-whispr/created-dialog/components/whispr-no-password.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import * as Alert from '$lib/components/ui/alert/index.js';
+	import TriangleAlert from '@lucide/svelte/icons/triangle-alert';
+</script>
+
+<Alert.Root
+	class="border-orange-300/30 bg-orange-500/10 dark:border-orange-400/30 dark:bg-orange-400/10"
+>
+	<TriangleAlert aria-hidden="true" />
+	<Alert.Title>No password set</Alert.Title>
+	<Alert.Description>
+		Everyone with access to the link can view and decrypt this whispr!
+	</Alert.Description>
+</Alert.Root>
--- src/lib/components/create-whispr/created-dialog/whispr-created-dialog.svelte
+++ src/lib/components/create-whispr/created-dialog/whispr-created-dialog.svelte
@@ -2,12 +2,12 @@
 	import { page } from '$app/state';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import type { CreatedWhispr } from '$lib/types/created-whispr';
-	import { Base64 } from 'js-base64';
 	import Lock from '@lucide/svelte/icons/lock';
-	import TriangleAlert from '@lucide/svelte/icons/triangle-alert';
+	import { Base64 } from 'js-base64';
 	import DialogDrawer from '../../common/dialog-drawer.svelte';
 	import WhisprAccordion from './components/whispr-accordion.svelte';
 	import WhisprLink from './components/whispr-link.svelte';
+	import WhisprNoPassword from './components/whispr-no-password.svelte';
 	import WhisprOverview from './components/whispr-overview.svelte';
 
 	// Props
@@ -50,19 +50,7 @@
 	{#snippet body()}
 		<div class="grid h-full items-start gap-2">
 			{#if randomPassword !== null}
-				<div
-					class="flex gap-3 rounded-lg border border-orange-300/30 bg-orange-500/10 p-3 text-orange-700 dark:border-orange-400/30 dark:bg-orange-400/10 dark:text-orange-300"
-					role="alert"
-					id="no-password-alert"
-				>
-					<TriangleAlert class="size-5 shrink-0" aria-hidden="true" />
-					<div class="space-y-1">
-						<p class="leading-none font-medium">No password set</p>
-						<p class="text-sm text-orange-700/90 dark:text-orange-300/90">
-							Everyone with access to the link can view and decrypt this whispr!
-						</p>
-					</div>
-				</div>
+				<WhisprNoPassword />
 			{/if}
 
 			<WhisprOverview {createdWhispr} />
