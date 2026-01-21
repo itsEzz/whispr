1301e4330938421a8a056b19b35647a825f53d57
itsEzz <github.swore664@passmail.net>
2026-01-21 20:04:10 +0100

    feat: update ESLint config to use defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,15 @@
-import prettier from 'eslint-config-prettier';
-import js from '@eslint/js';
 import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-export default ts.config(
+export default defineConfig(
 	includeIgnoreFile(gitignorePath),
 	{
 		ignores: ['src/lib/components/ui/**']
