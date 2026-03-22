0ed9adb3bc8c125b339815206216d370ceef340b
itsEzz <github.swore664@passmail.net>
2026-03-22 11:43:04 +0100

    refactor(db): use connection pool and inject schema

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,10 +2,15 @@ import { building } from '$app/environment';
 import { drizzle, MySql2Database } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import { serverAppConfig } from '../server-app-config';
+import * as schema from './schema';
 
-export let db: null | MySql2Database = null;
+export let db:
+	| null
+	| (MySql2Database<typeof schema> & {
+			$client: mysql.Pool;
+	  }) = null;
 
 if (!building) {
-	const client = await mysql.createConnection(serverAppConfig.DATABASE_URL);
-	db = drizzle(client);
+	const client = mysql.createPool(serverAppConfig.DATABASE_URL);
+	db = drizzle(client, { schema, mode: 'default' });
 }
